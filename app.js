cdb4a0c08da91255876f929302d13170480de4b2
rickfoust21 <rickfoust@gmail.com>
2017-05-25 13:56:30 -0700

    added questions to app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var cooking = confirm('Can I cook well?');
+if(cooking === true){
+  console.log ('I am great at cooking and Italian food is my specialty.');
+
+}else {
+  console.log ('Wrong. I can cook almost anything.');
+}
+
+var computer = confirm('Am I great at setting up my own laptop');
+if(computer === false){
+  console.log ('Correct. In fact I needed help from nearly half the class.');
+
+}else {
+  console.log ('HAHAHAHA. Yeah I wish...');
+}
+
+var compute = confirm('Am I great at setting up my own laptop');
+if(compute === false){
+  console.log ('Correct. In fact I needed help from nearly half the class.');
+
+}else {
+  console.log ('HAHAHAHa. Yeah I wish...');
+}
