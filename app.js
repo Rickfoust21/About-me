accd23aee7a5e99026cbf7871abc37bb154a5d27
rickfoust21 <rickfoust@gmail.com>
2017-06-13 20:28:17 -0700

    fixing lab2

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
 'use strict';
 
-var cooking = confirm('Can I cook well?');
-if(cooking === true){
-  console.log ('I am great at cooking and Italian food is my specialty.');
+var user = prompt('What is your name?');
+alert ('Hello ' + user + ' try to guess a few yes or no questions about me.');
 
-}else {
-  console.log ('Wrong. I can cook almost anything.');
+var correct = 0;
+console.log('Value of correct is ' + correct);
+
+
+
+//Question 1
+var cooking = prompt('Can I cook well?');
+if(cooking.toUpperCase() === 'YES') {
+  alert('I am great at cooking and Italian food is my specialty.');
+  correct++;
+  console.log (correct);
+} else {
+  alert('Wrong. I can cook almost anything.');
 }
 
-var computer = confirm('Am I great at setting up my own laptop');
-if(computer === false){
-  console.log ('Correct. In fact I needed help from nearly half the class.');
-
+//Question 2
+var computer = prompt('Am I great at setting up my own laptop');
+if(computer.toUpperCase() === 'NO') {
+  alert('Correct. In fact I needed help from nearly half the class.');
+  correct++;
+  console.log (correct);
 }else {
-  console.log ('HAHAHAHA. Yeah I wish...');
+  alert('HAHAHAHA. Yeah I wish...');
 }
 
-var compute = confirm('Am I great at setting up my own laptop');
-if(compute === false){
-  console.log ('Correct. In fact I needed help from nearly half the class.');
-
+//Question 3
+var Trek = prompt('Is Star Wars better than Star Trek?');
+if(Trek.toUpperCase() === 'YES') {
+  alert('You are incorrect. Star Trek is better. Resistance is Futile.');
+  correct++;
+  console.log (correct);
 }else {
-  console.log ('HAHAHAHa. Yeah I wish...');
+  alert('Correct! Star Trek is better and no one kisses their sister.');
+}
+
+//Question 4
+var travel = prompt('Have I been to every continent?');
+if(travel.toUpperCase() === 'NO') {
+  alert('Correct. I have never been to Antartica');
+  correct++;
+  console.log (correct);
+}else{
+  alert ('Wrong. Never been to Antartica, and Australia was my favorite.');
+}
+
+//Question 5
+var sing = prompt('Am I good at singing?');
+if(sing.toUpperCase() === 'YES') {
+  alert ('I have been called the song bird of my generation by those who have heard me.....THAT good.');
+}else{
+  alert ('Correct. Tried to do Karaoke once and I was so bad they actually turned the mic off in the middle of the song.');
+  correct++;
+  console.log (correct);
 }
